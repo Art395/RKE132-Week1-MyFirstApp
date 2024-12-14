696fce48652a4fe977018cd804ee1386c861b735
Art395 <artem.chuev@tktk.ee>
2024-12-14 19:23:09 +0200

    create new app

--- MyFirstApp/obj/Debug/net8.0/MyFirstApp.AssemblyInfo.cs
+++ MyFirstApp/obj/Debug/net8.0/MyFirstApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyFirstApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75de742eafd877bd17680eba600f9a13e198c2a3")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyFirstApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyFirstApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
